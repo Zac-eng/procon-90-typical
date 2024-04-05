57da0e23654169ef659e6e1df20718d60678b464
Zac-eng <zack.abten@gmail.com>
2024-04-05 11:33:17 +0900

    solved pb16

--- star2-3/pb_18/src/main.rs
+++ star2-3/pb_18/src/main.rs
@@ -1,6 +1,5 @@
 use proconio::input;
 use std::f64::consts::PI;
-use nalgebra::Vector3;
 
 fn main() {
     input! {
@@ -11,20 +10,15 @@ fn main() {
         q: usize,
         e: [f64;q],
     }
+    let radius = l / 2.;
     for time in e {
         //the coordinate of the Ferris wheel
-        let fw_y: f64 = -1. * l  * (2. * PI * time / t ).sin();
-        let fw_z: f64 = l * (1.0 - (2.0 * PI * time / t).cos());
-        //the vector describes Ferris wheel's position
-        let fw_vec = Vector3::new(0., fw_y, fw_z);
-        //the vector from Ferris wheel to Takahashi statue
-        let fw_t_vec = Vector3::new(x , y , 0.) - fw_vec;
-        let fw_t_abs = fw_t_vec.norm();
-        let sin = fw_z / fw_t_abs; 
-        // let ip = fw_t_vec.dot(&fw_t_x);
-        // let fw_t_x_abs = fw_t_x.norm();
-        // let cos = ip / fw_t_abs / fw_t_x_abs;
-        // println!("{}", cos.acos() * 360. / 2. / PI);
-        println!("{}", sin.asin() * 360. / 2. / PI);
+        let fw_y: f64 = -1. * radius  * (2. * PI * time / t ).sin();
+        let fw_z: f64 = radius * (1.0 - (2.0 * PI * time / t).cos());
+        
+        let vec_xy: [f64;2] = [x, y - fw_y];
+        let xy_abs = (vec_xy[0].powi(2) + vec_xy[1].powi(2)).sqrt();
+        let atan: f64 = fw_z.atan2(xy_abs);
+        println!("{}", atan * 360. / 2. / PI);
     }
 }
